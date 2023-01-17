90272f65d95939a748906ae582e28148b636df9b
Jeremy Hayes <jeremyhayes@users.noreply.github.com>
2023-01-16 20:32:46 -0500

    endpoint to capture airgradient push

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.get("/")
+def get_index():
     return "<p>Hello, World!</p>"
+
+@app.post("/sensors/airgradient:<chip_id>/measures")
+def post_sensor_measures(chip_id):
+    # TODO store this for later prometheus scrape
+    print(f"""DEBUG
+        chip_id={chip_id},
+        wifi={request.json['wifi']},
+        rco2={request.json['rco2']},
+        pm02={request.json['pm02']},
+        atmp={request.json['atmp']},
+        rhum={request.json['rhum']},
+        """, flush=True)
+
+    return ('', 201)
